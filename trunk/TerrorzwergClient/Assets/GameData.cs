0de8d459d2106af172fce0a9427d9fb4a80458cd
kartonriese@gmail.com <kartonriese@gmail.com@db51483f-8ce7-f8be-0d4c-8d83ae53a5c5>
2012-12-12 10:39:13 +0000

    Fixed some stuff.

--- trunk/TerrorzwergClient/Assets/GameData.cs
+++ trunk/TerrorzwergClient/Assets/GameData.cs
@@ -5,4 +5,9 @@ public class GameData : MonoSingleton< GameData >
     public string ipAdress = "";
 	public int playerId = 0;
     public override void Init(){ ipAdress="77.79.99.99"; playerId=0; }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
 }
