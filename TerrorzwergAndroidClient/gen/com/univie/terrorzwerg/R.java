0f28e1b952fe7174e79217a4ec8197d393de8cd1
patrik405 <patrik405@gmail.com>
2013-03-30 20:04:15 +0000

    added basic functionality to android client. Striking ... walking. now only server is needed - and some small adaptions

--- TerrorzwergAndroidClient/gen/com/univie/terrorzwerg/R.java
+++ TerrorzwergAndroidClient/gen/com/univie/terrorzwerg/R.java
@@ -35,14 +35,12 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int button=0x7f020000;
         public static final int connection_failed=0x7f020001;
         public static final int ic_launcher=0x7f020002;
-        public static final int menu_blue_hole=0x7f020003;
-        public static final int menu_hole=0x7f020004;
-        public static final int menu_red_hole=0x7f020005;
-        public static final int prepare_blue=0x7f020006;
-        public static final int prepare_red=0x7f020007;
-        public static final int striking_surface=0x7f020008;
-        public static final int striking_surface_wet=0x7f020009;
-        public static final int won=0x7f02000a;
+        public static final int menu_hole=0x7f020003;
+        public static final int prepare_blue=0x7f020004;
+        public static final int prepare_red=0x7f020005;
+        public static final int striking_surface=0x7f020006;
+        public static final int striking_surface_wet=0x7f020007;
+        public static final int won=0x7f020008;
     }
     public static final class id {
         public static final int action_settings=0x7f0a0005;
